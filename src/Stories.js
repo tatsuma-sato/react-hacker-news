fae2962eac0e0d6257f947c840ae9170e124e06f
tatsuma-sato <t.tsuma.s@gmail.com>
2022-01-10 10:41:32 -0800

    added features

--- src/Stories.js
+++ src/Stories.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
-import { useGlobalContext } from './context'
+import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  return <h2>stories component</h2>
-}
+  const { isLoading, hits, removeStory } = useGlobalContext();
 
-export default Stories
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="stories">
+      {hits.map((story) => {
+        const {
+          objectID: id,
+          title,
+          num_comments: comments,
+          url,
+          points,
+          author,
+        } = story;
+        return (
+          <article key={id} className="story">
+            <h4 className="title">{title}</h4>
+            <p className="inof">
+              {points} points by <span>{author} | </span>
+              {comments} comments
+            </p>
+            <div>
+              <a href={url} className="read-link">
+                read more
+              </a>
+              <button className="remove-btn" onClick={() => removeStory(id)}>
+                remove
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Stories;
